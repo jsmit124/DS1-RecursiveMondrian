c6b7e64c9d755bd76fc4d5a76487d44fe587ff23
jsmit124 <jsmit124@my.westga.edu>
2020-03-09 18:45:20 -0400

    dialogs working and skeleton built

--- src/edu/westga/cs3151/mondrian/MondrianArt.java
+++ src/edu/westga/cs3151/mondrian/MondrianArt.java
@@ -1,6 +1,8 @@
 package edu.westga.cs3151.mondrian;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Random;
 
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -22,26 +24,12 @@ public class MondrianArt extends Application {
 	public static final int WIDTH = 600;
 	public static final int HEIGHT = 600;
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			this.displayDialog();
+	private double minWidth;
+	private double minHeight;
+	private double maxLineWidth;
 
-			Canvas canvas = new Canvas(600, 600);
-			GraphicsContext gc = canvas.getGraphicsContext2D();
-			this.createGraphics(gc);
+	private Random randomIntGenerator = new Random(WIDTH);
 
-			Group group = new Group();
-			group.getChildren().add(canvas);
-			Scene scene = new Scene(new BorderPane(group), WIDTH, HEIGHT);
-			primaryStage.setTitle("Mondrian Art by Firstname Lastname");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	/**
 	 * Entry point
@@ -54,36 +42,103 @@ public class MondrianArt extends Application {
 		launch(args);
 	}
 
-	/**
-	 * Example code displaying a dialog to get user input
-	 */
-	private void displayDialog() {
-		TextInputDialog dialog = new TextInputDialog();
-		dialog.setTitle("Settings");
-		dialog.setHeaderText("Enter desired minimum region length: ");
-		dialog.setContentText("Length: ");
-		Optional<String> result = dialog.showAndWait();
-		if (result.isPresent()) {
-			System.out.println("You entered: " + result.get());
-		} else {
-			System.out.println("No length was returned by the dialog. Did you cancel or close the dialog?");
-		}
-		
+
+	@Override
+	public void start(Stage primaryStage) {
 		try {
-			Double.parseDouble(result.get());
-		} catch (NumberFormatException e) {
-			System.out.println("Invalid entry. Please enter a number.");
-			this.displayDialog();
+			this.minHeight = this.displayDialog("Enter desired minimum height: ", "Minimum height: ");
+			this.minWidth = this.displayDialog("Enter desired mimimum length: ", "Minimum width: ");
+			this.maxLineWidth = this.displayDialog("Enter desired maximum line width: ", "Maximum line width: ");
+
+			Canvas canvas = new Canvas(600, 600);
+			GraphicsContext gc = canvas.getGraphicsContext2D();
+
+			this.drawRectangle(gc, canvas.getWidth(), canvas.getHeight());
+
+			Group group = new Group();
+			group.getChildren().add(canvas);
+			Scene scene = new Scene(new BorderPane(group), WIDTH, HEIGHT);
+			primaryStage.setTitle("Mondrian Art by Justin Smith");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
+
 	/**
-	 * Example code creating a collection of rectangles
+	 * Example code displaying a dialog to get user input
 	 */
-	private void createGraphics(GraphicsContext gc) {
+	private double displayDialog(String header, String contentText) {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Settings");
+		dialog.setHeaderText(header);
+		dialog.setContentText(contentText);
+		Optional<String> result = dialog.showAndWait();
+		
+		try {
+			var input = Double.parseDouble(result.get());
+			if (input < 1) {
+				this.displayDialog("Enter a number higher than 0", contentText);
+			}
+		} catch (NumberFormatException e) {
+			return this.displayDialog("Invalid entry. Please enter a number.", contentText);
+		} catch (NoSuchElementException e) {
+			System.exit(0);
+		}
+
+		return Double.parseDouble(result.get());
+	}
+
+
+	/**
+	 * Recursive code creating a collection of rectangles
+	 */
+	private void drawRectangle(GraphicsContext gc, double width, double height) {
+		//setup 
+		gc.setStroke(Color.BLACK);
+		
+		//expansion: random line width
+		int randomLineWidth = this.randomIntGenerator.nextInt((int) this.maxLineWidth) + 1;
+		gc.setLineWidth(randomLineWidth);
+		
+		//determine if region should be split or not - DONE
+		if (width < this.minWidth && height < this.minHeight) {
+			return;
+		}
+		
+		//determine is region should be split vertically or horizontally
+		if (width < height) {
+			//draw horizontal line
+			//determine split of region randomly
+			//TODO
+		} else {
+			//draw vertical line
+			//determine split of region randomly
+			//TODO
+		}
+		
+		//draw random region and fill with random color
+		//generate random color for fill
+		int redByte = this.randomIntGenerator.nextInt(256);
+		int greenByte = this.randomIntGenerator.nextInt(256);
+		int blueByte = this.randomIntGenerator.nextInt(256);
+		Color randomColor = Color.rgb(redByte, greenByte, blueByte);
+		gc.setFill(randomColor);
+		
+		//recursively call this method
+		//TODO
+		
+		//expansions:
+		//	use pattern fill instead of solid fill
+		//TODO
+		
+		
 		gc.setFill(Color.BLUE);
 		gc.fillRect(0, 0, 300, 600);
-		
+
 		gc.setStroke(Color.WHITE);
 		gc.setLineWidth(20);
 		gc.strokeLine(290, 10, 290, 590);
@@ -93,9 +148,10 @@ public class MondrianArt extends Application {
 
 		gc.setFill(Color.YELLOW);
 		gc.fillRect(300, 300, 300, 300);
-		
+
 		gc.setStroke(Color.GREEN);
 		gc.setLineWidth(30);
 		gc.strokeLine(315, 300, 585, 300);
 	}
+
 }
